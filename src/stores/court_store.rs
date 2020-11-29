21adea3e9b752674b5322f2113bb119262305645
Nehliin <oskar.d.nehlin@gmail.com>
2020-11-29 15:25:50 +0100

    Add court store

--- src/stores/court_store.rs
+++ src/stores/court_store.rs
@@ -1,9 +1,10 @@
 #![allow(clippy::toplevel_ref_arg)]
 
+use async_trait::async_trait;
 use chrono::{Local, NaiveDateTime};
 use serde::Serialize;
 use sqlx::PgPool;
-
+use tracing::{error, warn};
 #[derive(Debug, sqlx::FromRow, Serialize)]
 pub struct CourtQueueEntry {
     pub place_in_queue: NaiveDateTime,
@@ -17,13 +18,42 @@ pub struct TournamentCourtAllocation {
     pub tournament_id: i32,
     pub match_id: Option<i64>,
 }
+// Court service?
+#[async_trait]
+pub trait CourtStore {
+    async fn insert_tournament_court_allocation(
+        &self,
+        tournament_court_allocation: TournamentCourtAllocation,
+    ) -> Result<(), sqlx::Error>;
 
-pub struct CourtStore {
-    pub pool: PgPool,
+    async fn get_match_court(
+        &self,
+        tournament_id: i32,
+        match_id: i64,
+    ) -> Result<Option<String>, sqlx::Error>;
+    // TODO: make this return result instead
+    async fn try_assign_free_court(
+        &self,
+        tournament_id: i32,
+        match_id: i64,
+    ) -> Result<Option<String>, sqlx::Error>;
+    async fn append_court_queue(
+        &self,
+        tournament_id: i32,
+        match_id: i64,
+    ) -> Result<(), sqlx::Error>;
+    async fn get_court_queue_placement(
+        &self,
+        tournament_id: i32,
+        match_id: i64,
+    ) -> Result<usize, sqlx::Error>;
+    async fn pop_court_queue(&self, _tournament_id: i32) -> Result<i64, sqlx::Error>;
 }
-
-impl CourtStore {
-    pub async fn insert_tournament_court_allocation(
+// TODO add tracing
+#[async_trait]
+impl CourtStore for PgPool {
+    #[tracing::instrument(name = "Inserting court -> tournament allocation", skip(self))]
+    async fn insert_tournament_court_allocation(
         &self,
         tournament_court_allocation: TournamentCourtAllocation,
     ) -> Result<(), sqlx::Error> {
@@ -32,25 +62,56 @@ impl CourtStore {
             tournament_court_allocation.court_name,
             tournament_court_allocation.tournament_id,
             tournament_court_allocation.match_id,
-            )
-            .execute(&self.pool)
-            .await?;
+        )
+        .execute(self)
+        .await
+        .map_err(|err| {
+            error!("Failed to allocate court to tournament: {}", err);
+            err
+        })?;
         Ok(())
     }
 
-    pub async fn get_match_court(&self, tournament_id: i32, match_id: i64) -> Result<Option<String>, sqlx::Error> {
-        todo!()
-    }
-
-    pub async fn try_assign_free_court(
+    #[tracing::instrument(name = "Fetching match court", skip(self))]
+    async fn get_match_court(
         &self,
-        _tournament_id: i32,
-        _match_id: i64,
-    ) -> Result<(), sqlx::Error> {
-        todo!()
+        tournament_id: i32,
+        match_id: i64,
+    ) -> Result<Option<String>, sqlx::Error> {
+        let row = sqlx::query!("SELECT court_name FROM tournament_court_allocation WHERE tournament_id = $1 AND match_id = $2", 
+            tournament_id,
+            match_id
+        )
+        .fetch_optional(self)
+        .await
+        .map_err(|err| {
+            error!("Failed fetch match court: {}", err);
+            err
+        })?;
+        Ok(row.map(|row| row.court_name))
     }
 
-    pub async fn append_court_queue(
+    #[tracing::instrument(name = "Trying to assign free court to match", skip(self))]
+    async fn try_assign_free_court(
+        &self,
+        tournament_id: i32,
+        match_id: i64,
+    ) -> Result<Option<String>, sqlx::Error> {
+        let row = sqlx::query!("UPDATE tournament_court_allocation SET match_id = $1 WHERE tournament_id = $2 AND match_id = NULL RETURNING court_name",
+            match_id,
+            tournament_id
+        )
+         .fetch_optional(self)
+         .await
+         .map_err(|err| {
+            error!("Failed assign free court: {}", err);
+            err
+        })?;
+        Ok(row.map(|row| row.court_name))
+    }
+
+    #[tracing::instrument(name = "Appending match to court queue", skip(self))]
+    async fn append_court_queue(
         &self,
         tournament_id: i32,
         match_id: i64,
@@ -61,12 +122,17 @@ impl CourtStore {
             match_id,
             tournament_id
         )
-        .execute(&self.pool)
-        .await?;
+        .execute(self)
+        .await
+        .map_err(|err| {
+            error!("Failed to append match to court queue");
+            err
+        })?;
         Ok(())
     }
 
-    pub async fn get_court_queue_placement(
+    #[tracing::instrument(name = "Fetch court queue placement", skip(self))]
+    async fn get_court_queue_placement(
         &self,
         tournament_id: i32,
         match_id: i64,
@@ -76,8 +142,12 @@ impl CourtStore {
             "SELECT match_id FROM court_queue WHERE tournament_id = $1 ORDER BY place_in_queue ASC LIMIT 100",
             tournament_id
         )
-        .fetch_all(&self.pool)
-        .await?;
+        .fetch_all(self)
+        .await
+        .map_err(|err| {
+            error!("Failed to fetch queue placement"); 
+            err
+        })?;
 
         if let Some(queue_index) = queue_entries
             .iter()
@@ -85,11 +155,35 @@ impl CourtStore {
         {
             Ok(queue_index + 1)
         } else {
+            error!("Match {} not found in court queue!", match_id);
             Err(sqlx::Error::RowNotFound)
         }
     }
 
-    pub async fn pop_court_queue(&self, _tournament_id: i32) -> Result<i64, sqlx::Error> {
-        todo!()
+    #[tracing::instrument(name = "Popping court queue", skip(self))]
+    async fn pop_court_queue(&self, tournament_id: i32) -> Result<i64, sqlx::Error> {
+        if let Some(head_of_queue) = sqlx::query!("SELECT match_id FROM court_queue WHERE tournament_id = $1 ORDER BY place_in_queue ASC LIMIT 1", 
+            tournament_id
+        )
+        .fetch_optional(self)
+        .await
+        .map_err(|err| {
+            error!("Failed to fetch head of court queue");
+            err
+        })? {
+           sqlx::query!("DELETE FROM court_queue WHERE tournament_id = $1 AND match_id = $2", 
+                tournament_id,
+                head_of_queue.match_id
+            )
+            .execute(self)
+            .await.map_err(|err| {
+                error!("Failed to delete head of court queue");
+                err
+            })?;
+            Ok(head_of_queue.match_id)
+        } else {
+            warn!("No court found in the court queue!");
+            Err(sqlx::Error::RowNotFound)
+        }
     }
 }
--- src/stores/mod.rs
+++ src/stores/mod.rs
@@ -1,5 +1,5 @@
+pub mod court_store;
 pub mod match_store;
 pub mod player_registration_store;
 pub mod player_store;
 pub mod tournament_store;
-pub mod court_store;
